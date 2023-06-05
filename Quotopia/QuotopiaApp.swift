c17bb601e7af66bc90d5e9e04508aa5c7f833087
amirintech <amir224030@bue.edu.eg>
2023-06-05 11:28:14 +0300

    Initial Commit

---
+++ Quotopia/QuotopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotopiaApp.swift
+//  Quotopia
+//
+//  Created by Amir on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuotopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
